693180e5db3e06bdd1254d2b1277198d9a047907
anshushrivastava67 <78591207+anshushrivastava67@users.noreply.github.com>
2022-12-13 21:42:55 +0530

    Update test.py

--- MEdium_of_two_sorted_array/test.py
+++ MEdium_of_two_sorted_array/test.py
@@ -1,3 +1,4 @@
+# Finding out the medium in the two sorted array
 a=[]
 n=int(input("Enter the size of list1:"))
 for i in range(n):
@@ -11,13 +12,13 @@ for i in range(m):
     a.append(s)
 c=a+b
 c.sort()
-# z=0
 x=len(c)
 print(c)
+# For the Even Lists
 if(x%2==0):
     med=float((c[(x-1)//2]+c[x//2]))/2.0
     print(med)
-
+# For the odd Lists
 else:
     med=(x+1)//2
     print(c[med-1])
